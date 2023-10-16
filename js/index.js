b53e286c51f65e7f4c1844bceaf8406ea0e2e49a
priyanshunagariya16 <priyanshunagaria16@gmail.com>
2023-10-16 21:02:07 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+const theme = document.getElementById("theme");
+const hamburger = document.getElementById("hamburger");
+const navbar = document.getElementById("navbar");
+const body = document.body;
+
+
+var preloader = document.getElementById("loading");
+function myFunc(){
+	preloader.style.display = "none";
+}
+
+theme.addEventListener("click", e => {
+	body.classList.toggle("dark-theme");
+	if (theme.innerText == "☀️") {
+		theme.innerText = "🌙";
+	} else {
+		theme.innerText = "☀️";
+	}
+});
+
+function changeColor(hue) {
+	document.documentElement.style.setProperty("--primary", hue);
+}
+
+navbar.onclick = removeSlide;
+body.onscroll = removeSlide;
+
+function removeSlide() {
+	navbar.classList.remove("slide");
+}
+
+hamburger.addEventListener("click", e => {
+	navbar.classList.toggle("slide");
+});
